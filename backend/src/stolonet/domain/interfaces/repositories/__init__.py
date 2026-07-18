021df7986deca60c9eee27c5231605c5fa3b84ce
Grigorii <griger1011@gmail.com>
2026-07-18 14:30:24 +0300

    feat: add repository for telemetry data

---
+++ backend/src/stolonet/domain/interfaces/repositories/__init__.py
@@ -0,0 +1,5 @@
+from stolonet.domain.interfaces.repositories.reading_repository import ReadingRepository
+
+__all__ = [
+    "ReadingRepository",
+]
---
+++ backend/src/stolonet/domain/interfaces/repositories/reading_repository.py
@@ -0,0 +1,9 @@
+from typing import Protocol
+
+from stolonet.domain.models import TelemetryEnvelope
+
+
+class ReadingRepository(Protocol):
+    async def save_telemetry_data(self, telemetry_data: TelemetryEnvelope) -> None: ...
+
+    async def get_telemetry_data_by_hours_window(self, node_id: str, hours=24) -> list[TelemetryEnvelope]: ...
