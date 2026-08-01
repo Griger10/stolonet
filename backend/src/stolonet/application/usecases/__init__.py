5ab02939682a3929324d8a19f5f0564f2ed27a1c
Grigorii <griger1011@gmail.com>
2026-08-01 11:17:43 +0300

    feat: add save telemetry usecase

---
+++ backend/src/stolonet/application/usecases/__init__.py
@@ -0,0 +1,6 @@
+from stolonet.application.usecases.save_telemetry_data import SaveTelemetryData
+
+
+__all__ = [
+    "SaveTelemetryData",
+]
---
+++ backend/src/stolonet/application/usecases/save_telemetry_data.py
@@ -0,0 +1,10 @@
+from stolonet.domain.interfaces.repositories import ReadingRepository
+from stolonet.domain.models import TelemetryEnvelope
+
+
+class SaveTelemetryData:
+    def __init__(self, reading_repo: ReadingRepository) -> None:
+        self._reading_repo = reading_repo
+
+    async def __call__(self, telemetry_data: TelemetryEnvelope) -> None:
+        await self._reading_repo.save_telemetry_data(telemetry_data)
