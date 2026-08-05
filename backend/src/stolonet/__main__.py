76338b3ff677c9d9503996616798d8fadc81982b
Grigorii <griger1011@gmail.com>
2026-08-05 12:30:23 +0300

    feat, fix: enable dishka integration for fastapi and faststream, add router for telemetry

--- backend/src/stolonet/__main__.py
+++ backend/src/stolonet/__main__.py
@@ -2,17 +2,22 @@ import asyncio
 import logging
 
 import uvicorn
+from dishka.integrations.fastapi import setup_dishka as setup_fastapi_dishka
+from dishka_faststream import setup_dishka as setup_faststream_dishka
 from fastapi import FastAPI
 from faststream.mqtt.fastapi import MQTTRouter
 
+from stolonet.api.routers.telemetry import telemetry_router
 from stolonet.bootstrap.config import Config
+from stolonet.bootstrap.di.ioc import create_container
 from stolonet.bootstrap.log import configure_logging
 from stolonet.ingest.registry import load_ingest_handlers, register_all
 
 logger = logging.getLogger(__name__)
 
+container = create_container()
 config = Config()
-broker = MQTTRouter(
+mqtt_router = MQTTRouter(
     host=config.mqtt_config.host,
     port=config.mqtt_config.port,
 )
@@ -25,11 +30,15 @@ app = FastAPI(
     redoc_url="/api/redoc" if config.api_config.debug else None,
 )
 
+setup_faststream_dishka(container=container, broker=mqtt_router.broker, auto_inject=True)
+setup_fastapi_dishka(container=container, app=app)
+
 
 async def run_api() -> None:
     load_ingest_handlers()
-    register_all(broker)
-    app.include_router(broker)
+    register_all(mqtt_router)
+    app.include_router(mqtt_router)
+    app.include_router(telemetry_router)
     server_config = uvicorn.Config(
         app=app, host=config.api_config.host, port=config.api_config.port
     )
