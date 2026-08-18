08e132fcbf72a6edeffaa8cc4e5b5c871e30a8d0
Grigorii <griger1011@gmail.com>
2026-08-18 22:04:01 +0300

    feat: added reading bme280 and soil moisture data from esp32

--- firmware/esp32-node/src/main.rs
+++ firmware/esp32-node/src/main.rs
@@ -1,10 +1,45 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use bme280::i2c::BME280;
+use esp_idf_svc::hal::adc::oneshot::config;
+use esp_idf_svc::hal::adc::{
+    oneshot::{AdcChannelDriver, AdcDriver},
+    ADC1,
+};
+use esp_idf_svc::hal::{
+    delay,
+    i2c::{I2cConfig, I2cDriver},
+    peripherals::Peripherals,
+    units::FromValueType,
+};
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+    let config = I2cConfig::new().baudrate(100_u32.kHz().into());
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
+
+    let mut bme280 = BME280::new_primary(i2c);
+    let mut delay = delay::Ets;
+    bme280.init(&mut delay).unwrap();
+
+    let adc = AdcDriver::new(peripherals.adc1).unwrap();
+    let mut adc_pin = AdcChannelDriver::new(
+        &adc,
+        peripherals.pins.gpio34,
+        &config::AdcChannelConfig::default(),
+    )
+    .unwrap();
+
+    loop {
+        let measurements = bme280.measure(&mut delay).unwrap();
+        log::info!("Temperature = {} °C", measurements.temperature);
+        log::info!("Humidity = {} %", measurements.humidity);
+        log::info!("Pressure = {} Pa", measurements.pressure);
+        let raw_value = adc.read(&mut adc_pin).unwrap();
+        log::info!("Soil moisture raw = {}", raw_value);
+        delay::FreeRtos::delay_ms(5000);
+    }
 }
